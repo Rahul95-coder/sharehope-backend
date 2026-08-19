b82b3b53471c99f61350f7d59e27c70a033e298c
Rahul <rahulshiv953727@gmail.com>
2026-08-19 18:42:53 +0530

    command commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+let port = 8080;
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
+
+app.use((req, res) => {
+    console.log("req received");
+});
