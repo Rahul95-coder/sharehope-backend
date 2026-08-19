0f43b5a718103b4a8bed2ceea063deb4c426facd
Yash <vajayash27092006@gmail.com>
2026-08-19 19:23:23 +0530

    User module added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const connectDB = require("./src/config/db");
 const app = express();
 let port = 8080;
+
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
 
-app.use((req, res) => {
-    console.log("req received");
+
+dotenv.config(); 
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Sharehope API is running 🚀');
 });
---
+++ src/user/user.model.js
@@ -0,0 +1,30 @@
+// backend/models/Product.js
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'User name is required'],
+            trim: true,
+        },
+        email: {
+            type: Number,
+            required: [true, 'User email is required'],
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'User password is required'],
+        },
+        role: {
+            type: String,
+            required: [true, 'User role is required'],
+        }
+    },
+    {
+        timestamps: true, // Automatically adds createdAt and updatedAt fields
+    }
+);
+
+module.exports = mongoose.model('User', productSchema);
