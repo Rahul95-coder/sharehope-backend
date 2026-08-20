0404f343513657cd9ed6137cd20dd290ebf3842e
Yash <vajayash27092006@gmail.com>
2026-08-20 21:53:33 +0530

    Sign up api added

--- src/auth/controller.js
+++ src/auth/controller.js
@@ -8,14 +8,77 @@ const signIn = async (req, res) => {
             is_deleted: false
         });
         console.log(user)
-        if(user.password === req.body.password){
-            res.status(200).json({email:user.email,password:user.password,role:user.role,})
+        if (user.password === req.body.password) {
+            res.status(200).json({ email: user.email, password: user.password, role: user.role, })
         }
-    } catch(error){
+    } catch (error) {
         res.status(401).json({ message: 'Failed to signin', error: error.message });
     }
 }
 
+// @desc   Sign up a  user
+//@route   POST /api/auth/signup
+const signUp = async (req, res) => {
+    try {
+        const { name,
+            role,
+            email,
+            phone,
+            password,
+            contactPersonName,
+            address,
+            city,
+            state,
+            pincode,
+            donorType,
+            registrationNumber,
+        } = req.body;
+
+        const existingUser = await User.findOne({
+            is_deleted: false,
+            $or: [
+                { name: name },
+                { email: email },
+                { phone: phone }
+            ]
+        });
+
+        if (existingUser) {
+            return res.status(409).json({
+                message: "User already exists with this name, email, or phone."
+            });
+        }
+
+        const user = new User({
+            name,
+            role,
+            email,
+            phone,
+            password,
+            contact_person_name: contactPersonName,
+            address,
+            city,
+            state,
+            pincode,
+            donor_type: donorType,
+            registration_number: registrationNumber
+        });
+
+        const savedUser = await user.save();
+
+        return res.status(201).json(savedUser);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to create user",
+            error: error.message,
+        });
+    }
+
+
+}
+
 module.exports = {
-    signIn
+    signIn,
+    signUp
 }
--- src/auth/routes.js
+++ src/auth/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { signIn} = require("./controller");
+const { signIn, signUp} = require("./controller");
 const router = express.Router();    
 
 router.post("/signin",signIn);
+router.post("/signup",signUp);
+
 
 module.exports = router;
